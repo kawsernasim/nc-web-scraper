8ef98943ec98764aead4027763d62af06d29314b
dyridantudk <dyridantudK@gmail.com>
2021-04-05 00:41:13 +0600

    the updated pipeline

--- paperscraper/paperscraper/pipelines.py
+++ paperscraper/paperscraper/pipelines.py
@@ -5,8 +5,20 @@
 
 
 # useful for handling different item types with a single interface
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 from db_helper import *
+from random import randint
+
+
+
+
 
 class PaperscraperPipeline:
     def __init__(self):
@@ -25,14 +37,19 @@ class PaperscraperPipeline:
 
         #split title into specific db columns (year, session, subject etc)
 
-        id = rand_gen()
+        id = randint(0, 1000)
         board = "Edexcel"
         qualification = "IGCSE"
         subject = "Mathematics B"
-        title_split = self.title.split(" ")
+        title_grab = self.title[0]
+        title_split = title_grab.split(' ')
         year = title_split[1]
         session = title_split[0]
-
+        paper_type = 1
+        a = self.link
+        for i in a:
+            link = i
+        
         return self.db.add_paper(id, board, qualification, subject, year, session, paper_type, link)
 
 
